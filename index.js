70ccd9ee7dd42a7eb56e6c56a8ec6a01545a40c6
shakurbol <bolakokbol@gmail.com>
2023-10-04 00:49:44 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML = data.id; 
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message; 
+      });
+  }
+  
